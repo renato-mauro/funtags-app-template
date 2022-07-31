50e393ee8d9836261148b41aed82fd3e54b2a78a
renato-mauro <renato.mauro@gmail.com>
2022-07-30 21:15:35 -0300

    App Development

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import ft from 'funtags';
+import { sideBar } from './templates/SideBar.js';
+import { loremIpsum } from './templates/LoremIpsum.js';
 
-function teste()
-{
-    let { div, h1, h2 } = ft.html;
-    return div(
-        h1("Hello"),
-        h2("World")
-    )
-}
 
-document.body.append(teste());
+document.querySelector("#sideBarPlaceholder").replaceChildren(sideBar())
+document.querySelector("#contentPlaceholder").replaceChildren(loremIpsum())
